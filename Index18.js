c1093e5d82a6b6cb715c9f3959b3754e02bcd554
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-04-12 15:13:23 +0530

    ok

--- Index18.js
+++ Index18.js
@@ -34,3 +34,11 @@ console.log(a+b);
 console.log(a/b);
 console.log(b); 
 console.log(a+b);
+
+
+console.log(a/b);
+console.log(b); 
+console.log(a+b);
+console.log(a/b);
+console.log(b); 
+console.log(a+b);
