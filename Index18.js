c6acd7794b23481c0b44aa87e50376db454570d6
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-04-20 18:56:25 +0530

    kkg

--- Index18.js
+++ Index18.js
@@ -99,3 +99,17 @@ console.log(a+b);
 console.log(a);
 console.log(b);
 
+console.log(a+b);
+console.log(a/b);
+console.log(b); 
+console.log(a+b);
+console.log(a/b);
+console.log(b); 
+console.log(a+b);
+
+console.log(a);
+console.log(b);
+
+
+
+
