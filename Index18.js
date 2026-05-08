49854b53dd758a259b4ec6321066dd12de19d017
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-05-08 21:52:51 +0530

    nnk

--- Index18.js
+++ Index18.js
@@ -91,3 +91,11 @@ console.log(a);
 console.log(b);
 console.log(a);
 console.log(b);
+
+console.log(b);
+console.log(a);
+console.log(b);
+console.log(a);
+console.log(b);
+console.log(a);
+console.log(b);
