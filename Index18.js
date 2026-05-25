870b596713a0dda2add35d82993c2ea006097d96
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-05-25 22:22:00 +0530

    Add duplicate console log statements for variable tracking

--- Index18.js
+++ Index18.js
@@ -31,3 +31,13 @@ console.log(b);
 console.log(b);
 console.log(a);
 console.log(b);
+
+
+let b=10
+b=10
+console.log(b);
+console.log(a);
+console.log(b);
+console.log(b);
+console.log(a);
+console.log(b);
