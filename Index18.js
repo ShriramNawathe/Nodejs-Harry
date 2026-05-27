78bdef73b3eeebb5abfeda2249f66684ca3c2a79
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-05-27 22:11:23 +0530

    Add additional console log statements for variable tracking

--- Index18.js
+++ Index18.js
@@ -50,3 +50,12 @@ console.log(b);
 console.log(b);
 console.log(a);
 console.log(b);
+
+let b=10
+b=10
+console.log(b);
+console.log(a);
+console.log(b);
+console.log(b);
+console.log(a);
+console.log(b);
